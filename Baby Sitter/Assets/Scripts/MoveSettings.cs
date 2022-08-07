c6a1c3fcdda8bd18a6b7e63b68253df1238c0ffa
SwivelDev <ttvbrybry@gmail.com>
2022-08-07 13:06:59 -0700

    Add Scripts Folder and Player Movement Scripts

---
+++ Baby Sitter/Assets/Scripts/MoveSettings.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(menuName = "Scriptable_Objects/Movement/Settings")]
+public class MoveSettings : ScriptableObject
+{
+    public float speed { get { return _speed; } private set { _speed = value; } }
+    [SerializeField] private float _speed = 5.0f;
+
+    public float jumpForce { get { return _jumpForce; } private set { _jumpForce = value; } }
+    [SerializeField] private float _jumpForce = 13.0f;
+
+    public float antiBump { get { return _antiBump; } private set { _antiBump = value; } }
+    [SerializeField] private float _antiBump = 4.5f;
+
+    public float gravity { get { return _gravity; } private set { _gravity = value; } }
+    [SerializeField] private float _gravity = 30.0f;
+}
---
+++ Baby Sitter/Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    [SerializeField] private MoveSettings _settings = null;
+
+    private Vector3 _moveDirection;
+    private CharacterController _controller;
+
+    private void Awake()
+    {
+        _controller = GetComponent<CharacterController>();
+    }
+
+    private void Update()
+    {
+        DefaultMovement();
+    }
+
+    private void FixedUpdate()
+    {
+        _controller.Move(_moveDirection * Time.deltaTime);
+    }
+
+    private void DefaultMovement()
+    {
+        if (_controller.isGrounded)
+        {
+            Vector2 input = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+
+            if (input.x != 0 && input.y != 0)
+            {
+                input *= 0.777f;
+            }
+
+            _moveDirection.x = input.x * _settings.speed;
+            _moveDirection.z = input.y * _settings.speed;
+            _moveDirection.y = -_settings.antiBump;
+
+            _moveDirection = transform.TransformDirection(_moveDirection);
+
+            if (Input.GetKey(KeyCode.Space))
+            {
+                Jump();
+            }
+        }
+        else
+        {
+            _moveDirection.y -= _settings.gravity * Time.deltaTime;
+        }
+    }
+
+    private void Jump()
+    {
+        _moveDirection.y += _settings.jumpForce;
+    }
+}
